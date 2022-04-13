118fd93f1307ad90be2d81928abcc102de8b9eac
arturbchagas <arturbbcc@gmail.com>
2022-04-13 12:54:42 -0300

    resumo

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const menu = document.querySelector(".menu-lateral");
 botaoMenu.addEventListener('click',()=>{
     menu.classList.toggle('menu-lateral--ativo');
 });
+
+let botaoResumo = document.querySelector(".botao-resumo");
+let resumo = document.querySelector(".resumo__texto-oculto");
+
+
+botaoResumo.addEventListener('click',()=>{
+    resumo.classList.remove('desativar',);
+    var pai = botaoResumo.parentNode;
+    pai.removeChild(botaoResumo);
+});
+
+
+
